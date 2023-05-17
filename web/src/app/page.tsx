a319976cffc408e954905a298047aed3c96fc33b
mteusbarbosa <mateus.bm13@gmail.com>
2023-05-16 22:53:41 -0300

    added stripes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,9 +2,11 @@ export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Esquerda */}
-      <div className="relative flex flex-col items-start justify-between px-28 py-16">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/* Stripe */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
       </div>
 
       {/* Direita */}
@@ -12,7 +14,7 @@ export default function Home() {
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
             Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="/" className="underline hover:text-gray-50">
+            <a href="" className="underline hover:text-gray-50">
               criar agora
             </a>
             !
